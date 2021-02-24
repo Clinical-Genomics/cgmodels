3972e231780fa01ed01fabc7d6c144d6ca4b4c27
moonso <mans.magnusson@scilifelab.se>
2021-02-24 22:03:33 +0100

    Make even more compact

--- cgmodels/demultiplex/sample_sheet.py
+++ cgmodels/demultiplex/sample_sheet.py
@@ -50,8 +50,7 @@ def get_sample_sheet(infile: Path, sheet_type: Literal["2500", "SP", "S2", "S4"]
     with open(infile, "r") as csv_file:
         # Skip the [data] header
         next(csv_file)
-        csv_reader = csv.DictReader(csv_file)
-        raw_samples: List[dict] = [row for row in csv_reader]
+        raw_samples: List[dict] = [row for row in csv.DictReader(csv_file)]
         sample_type = Sample if sheet_type == "2500" else NovaSeqSample
         samples = parse_obj_as(List[sample_type], raw_samples)
     validate_unique_sample(samples)
