d8b122a9ff6100ef363b6cb4551f35e106711113
ivadym <vadym.ivanchuk@scilifelab.se>
2022-07-22 10:35:23 +0200

    Add Balsamic PON pipeline option (#14)
    ### Added:
    - BALSAMIC-PON to pipeline options

--- cgmodels/cg/constants.py
+++ cgmodels/cg/constants.py
@@ -10,6 +10,7 @@ class Pipeline(StrEnum):
     BALSAMIC: str = "balsamic"
     BALSAMIC_QC: str = "balsamic-qc"
     BALSAMIC_UMI: str = "balsamic-umi"
+    BALSAMIC_PON: str = "balsamic-pon"
     DEMULTIPLEX: str = "demultiplex"
     FASTQ: str = "fastq"
     FLUFFY: str = "fluffy"
